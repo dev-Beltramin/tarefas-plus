e85f899d987e90980574fea320632a8d87216560
dev-Beltramin <paulobeltramin.27@gmail.com>
2024-06-19 09:34:33 -0300

    iniciando layout do perfil de usuario

---
+++ src/pages/profile.tsx
@@ -0,0 +1,20 @@
+
+
+
+
+import styles from '../../styles/profile.module.css'
+import Header2 from './components/header/header2'
+
+
+
+
+
+const Profile = () => {
+  return (
+    <div>
+        <Header2/>
+    </div>
+  )
+}
+
+export default Profile
