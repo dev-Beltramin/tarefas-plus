36aed55f1bfd29f6991f42dd57f93f25a6ff3d74
dev-Beltramin <paulobeltramin.27@gmail.com>
2024-06-20 01:42:59 -0300

    efetuado integração do projeto com o banco de dados do fire base , e criando logica para enviar os dados para o banco e listar no perfil do usuario , alem de privar as rotas para o perfil

---
+++ src/pages/services/db.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyA6cL0-UPDvTqBL5z-pedmo2SxHHZFmnbo",
+  authDomain: "tarefas-plus-e5811.firebaseapp.com",
+  projectId: "tarefas-plus-e5811",
+  storageBucket: "tarefas-plus-e5811.appspot.com",
+  messagingSenderId: "900463185673",
+  appId: "1:900463185673:web:46909862066faf97b70dd8"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -1,12 +1,92 @@
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  where
+} from "firebase/firestore";
 import { MdDelete } from "react-icons/md";
 
 import styles from "../../styles/tasks.module.css";
 import Header1 from "./components/header/header1";
-import { GetServerSideProps } from "next";
-import { getSession } from "next-auth/react";
 import { FiShare2 } from "react-icons/fi";
+import { db } from "./services/db";
 
-const Tasks = () => {
+interface userProps {
+  user: {
+    email: string;
+  };
+}
+
+interface tasksProps {
+  id: string;
+  created: Date;
+  Public: boolean;
+  tarefa: string;
+}
+
+const Tasks = ({ user }: userProps) => {
+  const [input, setInput] = useState("");
+  const [checked, setChecked] = useState(false);
+  const [tasks, setTasks] = useState<tasksProps[]>([]);
+
+  const handleChecked = (e: ChangeEvent<HTMLInputElement>) => {
+    setChecked(e.target.checked);
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    if (input === "") return;
+
+    try {
+      await addDoc(collection(db, "tarefas"), {
+        tarefa: input,
+        created: new Date(),
+        user: user.email,
+        Public: checked
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    setInput("");
+    setChecked(false);
+  };
+
+  useEffect(() => {
+    const listTasks = async () => {
+      let taskCollection = collection(db, "tarefas");
+
+      const buscar = query(
+        taskCollection,
+        orderBy("created", "desc"),
+        where("user", "==", user?.email)
+      );
+
+      onSnapshot(buscar, (snapshot) => {
+        let list = [] as tasksProps[];
+
+        snapshot.forEach((doc) => {
+          list.push({
+            id: doc.id,
+            created: doc.data().created,
+            tarefa: doc.data().tarefa,
+            Public: doc.data().Public
+          });
+        });
+
+        setTasks(list);
+      });
+    };
+
+    listTasks();
+  }, [user?.email]);
+
   return (
     <div className={styles.body}>
       <Header1 />
@@ -15,13 +95,24 @@ const Tasks = () => {
         <div className={styles.content}>
           <h2>Qual sua tarefa?</h2>
 
-          <textarea placeholder="Digite sua tarefa" />
-          <div className={styles.input}>
-            <input type="checkbox" name="public" />
-            <p>Deixar tarefa publica</p>
-          </div>
+          <textarea
+            placeholder="Digite sua tarefa"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <form onSubmit={handleSubmit}>
+            <div className={styles.input}>
+              <input
+                type="checkbox"
+                name="public"
+                checked={checked}
+                onChange={handleChecked}
+              />
+              <p>Deixar tarefa publica</p>
+            </div>
 
-          <button>Registrar</button>
+            <button>Registrar</button>
+          </form>
         </div>
       </section>
 
@@ -29,26 +120,28 @@ const Tasks = () => {
         <div className={styles.tasks}>
           <h2>Minhas Tarefas</h2>
 
-          <div className={styles.text}>
-            <div>
-              <span>
-                publico
+          {tasks.map((item) => (
+            <>
+              <div className={styles.text} key={item.id}>
+                <div>
+                  {item.Public && (
+                    <span>
+                      {item.Public}
+                      publico
+                      <i>
+                        <FiShare2 />
+                      </i>
+                    </span>
+                  )}
+                </div>
+                <p>{item.tarefa}</p>
                 <i>
-                  <FiShare2 />
+                  {" "}
+                  <MdDelete size={24} />
                 </i>
-              </span>
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil ad
-              magni exercitationem, architecto quas deserunt ducimus vitae a
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil ad
-              magni exercitationem, architecto quas deserunt ducimus vitae a
-            </p>
-            <i>
-              {" "}
-              <MdDelete size={24} />
-            </i>
-          </div>
+              </div>
+            </>
+          ))}
         </div>
       </section>
     </div>
@@ -68,7 +161,11 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   }
 
   return {
-    props: {}
+    props: {
+      user: {
+        email: session.user.email
+      }
+    }
   };
 };
 
